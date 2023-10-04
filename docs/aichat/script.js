18b62d25fbd06cc33612d168a14dcab691d4cfea
Perrin Hillam <perrinhillam@gmail.com>
2023-10-04 17:05:39 -0600

    Changed some text to be more specific, and changed to new server API. Also updated some CSS

--- docs/aichat/script.js
+++ docs/aichat/script.js
@@ -7,7 +7,7 @@ const socket = new WebSocket('wss://ttscrdlv-8080.usw3.devtunnels.ms/');
 // Event listener for when the WebSocket connection is opened
 socket.addEventListener('open', (event) => {
     socket.send(JSON.stringify({clientType: "User"}));
-    chatBox.innerHTML += '<div>Connected to the AI NameServer.</div>';
+    chatBox.innerHTML += '<div>Connected to the AI Server.</div>';
 });
 
 // Event listener for incoming messages from the server
@@ -18,8 +18,12 @@ socket.addEventListener('message', (event) => {
     chatBox.innerHTML += `<div class="message"><div class="name-server">${name}</div>: ${message}</div>`;
 });
 
+socket.addEventListener('error', (event) => {
+    chatBox.innerHTML += `<div>Error with AI server communication.</div>`;
+})
+
 socket.addEventListener('close', (event) => {
-    chatBox.innerHTML += `<div>Disconnected From Server!</div>`;
+    chatBox.innerHTML += `<div>Disconnected From AI Server! This could be because of an error, or maybe the server isn't ready right now.</div>`;
 });
 
 messageInput.addEventListener('keyup', (event) => {
@@ -27,6 +31,7 @@ messageInput.addEventListener('keyup', (event) => {
         const messageText = messageInput.value;
         if (messageText.trim() !== '') {
             const messageData = {
+                action: "message",
                 message: messageText
             };
             // Send the message to the server in JSON format
@@ -36,6 +41,7 @@ messageInput.addEventListener('keyup', (event) => {
             chatBox.innerHTML += `<div class="message"><div class="name-you">You: </div>${messageText}</div>`;
             
             // Clear the input field
+            messageInput.disabled = true;
             messageInput.value = '';
         }
     }
